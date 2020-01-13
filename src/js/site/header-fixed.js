5b515f4021d26e4ebe77a88be70b854eb1a58941
Anton Grakhov <ant.grakhov@gmail.com>
2020-01-14 03:42:47 +0500

    Add IntersectObserver fallback

--- src/js/site/header-fixed.js
+++ src/js/site/header-fixed.js
@@ -1,20 +1,30 @@
-if (
-	'IntersectionObserver' in window &&
+let scrollClass   = '_scrolled',
+	observerClass = 'header-observer'
+
+document.body.insertAdjacentHTML('beforeend', `<div class="${observerClass}"/>`)
+
+if ( 'IntersectionObserver' in window &&
 	'IntersectionObserverEntry' in window &&
 	'intersectionRatio' in window.IntersectionObserverEntry.prototype
 	) {
 
-	let scrollClass   = '_scrolled',
-		observerClass = 'header-observer',
-		observer      = new IntersectionObserver(entries => {
-			if (entries[0].boundingClientRect.y < 0) {
-				document.body.classList.add(scrollClass)
-			} else {
-				document.body.classList.remove(scrollClass)
-			}
-		})
-
-	document.body.insertAdjacentHTML('beforeend', `<div class="${observerClass}"/>`)
+	let observer = new IntersectionObserver(entries => {
+		if (entries[0].boundingClientRect.y < 0) {
+			document.body.classList.add(scrollClass)
+		} else {
+			document.body.classList.remove(scrollClass)
+		}
+	})
 
 	observer.observe(document.querySelector(`.${observerClass}`))
+}
+else {
+	$(window).scroll(function() {
+		if ( $(window).scrollTop() > 5 ) {
+			document.body.classList.add(scrollClass)
+		}
+		else {
+			document.body.classList.remove(scrollClass)
+		}
+	})
 }
