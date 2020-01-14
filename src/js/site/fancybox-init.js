7fcffc3e29e325e56d108f49b7601d028bc8e874
Anton Grakhov <ant.grakhov@gmail.com>
2020-01-14 17:55:21 +0300

    Init Sign In / Sign Up popups, other changes

--- src/js/site/fancybox-init.js
+++ src/js/site/fancybox-init.js
@@ -1,5 +1,8 @@
 import '@fancyapps/fancybox'
 
+/**
+ * Popup Search
+ */
 let $menu      = $('.header-menu-container'),
 	$tabletNav = $('.header-nav-tablet *')
 
@@ -13,3 +16,33 @@ $('[data-src="#popup-search"]').fancybox({
 		$menu.fadeIn()
 	}
 })
+
+
+/**
+ * Popup Sign In / Sign Up
+ */
+$('[data-src="#popup-sign"]')
+	.on('click', function(e){
+		let button = $(this)
+
+		if ( button.hasClass('_clicked') ) {
+			e.preventDefault()
+
+			$.fancybox.close()
+
+			button.removeClass('_clicked')
+		}
+		else {
+			button.addClass('_clicked')
+		}
+	})
+	.fancybox({
+		touch: false,
+		beforeShow: function( instance, current ) {
+			let $container = instance.$refs.container,
+				$slide     = instance.$refs.stage.find('.fancybox-slide')
+
+			$container.addClass('popup-sign-container')
+			$slide.addClass('d-flex')
+		}
+	})
