2f603c23b7453862a0379cbb9be0d31b40ec9b71
Anton Grakhov <ant.grakhov@gmail.com>
2020-02-03 13:50:29 +0500

    Add folders to Distributors table

---
+++ src/js/site/tables.js
@@ -0,0 +1,12 @@
+$('.table-folders').each(function(){
+	let table = $(this),
+		toggles = table.find('.table-folders-toggle')
+
+	toggles.on('click', function(){
+		let toggle = $(this),
+			folder = toggle.parent().parent().next()
+
+		toggle.toggleClass('_active')
+		folder.toggleClass('d-none')
+	})
+})
